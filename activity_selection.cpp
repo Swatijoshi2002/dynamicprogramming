a4d3c60544362907d8d64f83898f167a14a6951a
Swatijoshi2002 <79635662+Swatijoshi2002@users.noreply.github.com>
2022-06-03 16:22:52 +0530

    Create activity_selection.cpp

---
+++ activity_selection.cpp
@@ -0,0 +1,56 @@
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+vector<int> res;
+static bool compare(pair<int,int> &s1,pair<int,int>s2)
+{
+    if(s1.second>s2.second)
+    return false;
+    else return true;
+}
+void activity_selection(vector<int>& start,vector<int>& finish,int n)
+{
+    vector<pair<int,int>> v;
+    for(int i=0;i<n;i++)
+    {
+        v.push_back({start[i],finish[i]});
+    }
+    sort(v.begin(),v.end(),compare);
+   /* for(int i=0;i<n;i++)
+    {
+        cout<<v[i].second<<" ";
+    }*/
+    //activity selection
+    res.push_back(0);
+    
+    for(int i=1;i<n;i++)
+    {
+        int j=res[res.size()-1];
+        if(v[i].first>v[j].second)
+        {
+            res.push_back(i);
+         
+        }
+    }
+     for(int i=0;i<res.size();i++)
+    {
+        cout<<res[i]<<" ";
+    }
+}
+int main()
+{
+   int n;
+   cin>>n;
+   vector<int> s(n),f(n);
+   for(int i=0;i<n;i++)
+   {
+       cin>>s[i];
+   }
+   for(int i=0;i<n;i++)
+   {
+       cin>>f[i];
+   }
+   activity_selection(s,f,n);
+}
